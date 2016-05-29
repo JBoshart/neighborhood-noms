82a6d1e113975e9b13819b132320133b9e383647
Jillian Boshart <Jilliliz@gmail.com>
2016-05-28 19:39:01 -0700

    New rails app, some models and controllers added.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :require_login, only: [:show, :edit, :update]
+
+  # Create new user shell for form
+  def new
+    @user = User.new
+  end
+
+  # Save user provided informtion to the database, direct to login if successfull
+  def create
+    @user = User.new(user_create_params[:user])
+    if @user.save
+      redirect_to login_path
+    else
+      render :new
+    end
+  end
+
+  # Show user profile page, with maps and badges
+  def show
+    @maps = Map.find_by(user_id: current_user.id)
+    @badges = Badge.find_by(id: current_user.badge_ids)
+    render :show
+  end
+
+  # Edit user profile page
+  def edit
+    @user = User.find(current_user.id)
+    render :edit
+  end
+
+  # Save edited changes
+  def update
+    @user = User.find(current_user.id)
+    @user.update(user_create_params[:user])
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def user_create_params
+    params.permit(user: [:user_name, :name, :email, :password, :password_confirmation, :address, :city, :state, :zip])
+  end
+end
---
+++ app/models/nom.rb
@@ -0,0 +1,31 @@
+class Nom < ActiveRecord::Base
+  require 'TunesTakeoutWrapper'
+
+class Food < ActiveRecord::Base
+  YELP = Yelp::Client.new({ consumer_key: ENV["YELP_CONSUMER_KEY"],
+                              consumer_secret: ENV["YELP_CONSUMER_SECRET"],
+                              token: ENV["YELP_TOKEN"],
+                              token_secret: ENV["YELP_TOKEN_SECRET"]})
+
+  BASE_URL = "https://api.yelp.com/v2/"
+  attr_reader :business_id, :name, :url, :image_url, :phone, :rating
+
+  def initialize(suggestion_data)
+    @business_id = suggestion_data.business.id
+    @name = suggestion_data.business.name
+    @url = suggestion_data.business.url
+    @image_url = suggestion_data.business.image_url if !suggestion_data.business.image_url.nil?
+    @rating = suggestion_data.business.rating_img_url
+    @phone = suggestion_data.business.phone
+    @rating = suggestion_data.business.rating
+  end
+
+  def self.find(food_pairs)
+    food = []
+    food_pairs.each do |food_item|
+      item_found = YELP.business(Addressable::URI.parse(food_item.food_id).normalize.to_s)
+      food << self.new(item_found)
+    end
+    return food
+  end
+end
